6091e442d3beb87663c14a1eea7372f154808185
Vyan2008 <71358948+Vyan2008@users.noreply.github.com>
2021-01-27 16:59:38 -0600

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class box {
+    constructor(x,y,width,height) {
+      var options = {
+          restitution:0.4,
+          friction:100
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("blue");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
